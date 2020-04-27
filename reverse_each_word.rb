cca2d70c49dbe10c476726fbe62949c4dca64462
steven <bbtrombone@sbcglobal.net>
2020-04-27 02:22:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word(string)
-string =%W(#{string})
-new_home = [ ]
-empty = ""
-string.collect do |e|
-  e.reverse
-  new_home.unshift(e) 
-  end 
-new_home.to_s
+for i in string
+  puts i 
 end
